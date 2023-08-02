c10496df0625815fa2f594bcf45b76bd35b788c4
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-08-02 20:40:08 +0900

    リファクタリング

--- youtube_api/youtube_api/urls.py
+++ youtube_api/youtube_api/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('authen/', include('djoser.urls.jwt')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("authen/", include("djoser.urls.jwt")),
 ]
 
 if settings.DEBUG:
