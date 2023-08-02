79961f9ec67441a48111ba8d5cee66a005c01b78
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-08-02 19:54:41 +0900

    リファクタリング

--- youtube_api/api/serializers.py
+++ youtube_api/api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import User, Video
+from .models import Video
 
 
 class UserSerializer(serializers.ModelSerializer):
