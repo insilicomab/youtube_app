1a72673e085171ca74492943643b6c1f73530dd4
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-08-14 20:09:14 +0900

    ApiContextを追加

--- frontend/youtube_react/src/App.js
+++ frontend/youtube_react/src/App.js
@@ -1,9 +1,9 @@
 import { MuiThemeProvider } from "@material-ui/core";
-import "./App.css";
-
 import indigo from "@material-ui/core/colors/indigo";
 import createTheme from "@material-ui/core/styles/createTheme";
+import "./App.css";
 import NavBar from "./components/NavBar";
+import ApiContextProvider from "./context/ApiContext";
 
 const theme = createTheme({
   palette: {
@@ -19,9 +19,11 @@ const theme = createTheme({
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <NavBar />
-    </MuiThemeProvider>
+    <ApiContextProvider>
+      <MuiThemeProvider theme={theme}>
+        <NavBar />
+      </MuiThemeProvider>
+    </ApiContextProvider>
   );
 }
 
